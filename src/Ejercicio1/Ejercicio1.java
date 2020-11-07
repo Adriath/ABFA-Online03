ec3e724a32e0ed613dd1243e29f4e1c4c4817fc8
adria <adria@WassPC.home>
2020-11-07 20:22:27 +0100

    Creación del repositorio tarea online 3.

---
+++ src/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio1;
+
+/**
+ * A partir de la tarea online creada en la unidad didáctica 2, crea diferentes
+ * métodos, unos por ejercicio, que reciban como parámetros los datos que sean 
+ * necesarios para resolver el problema planteado y devuelvan el resultado.
+ * Debes de:
+ * 
+ * 1. Elegir correctamente los nombres de los métodos para que sean descriptivos.
+ *  Recuerda que deben comenzar por una letra minúscula y si tiene más de dos
+ *  palabras la inicial de la segunda, y sucesivas, estará en mayúscula.
+ * 
+ * 2. Crea un menú en el método principal que permita llamar a cada uno de los
+ *  métodos generados con los parámetros que necesiten y que muestre el resultado
+ *  devuelto por el método. Este menú se mostra´ra después de cada llamada a un
+ *  método hasta que se pulse el número 0 que nos permitirá salir del mismo.
+ * 
+ * 3. Utiliza try-catch para catpurar las posibles excepciones que se pueden dar.
+ * 
+ * @author Adrián Arjona
+ * @version 1.0
+ */
+public class Ejercicio1 {
+    
+    
+    
+}
