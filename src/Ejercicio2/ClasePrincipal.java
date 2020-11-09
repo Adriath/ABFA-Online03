935938ccb7f71d07d0b0dcb887be294993303952
adria <adria@WassPC.home>
2020-11-10 00:22:41 +0100

    Creación del ejercicio 2. He craado ambas clases y todos los métodos. El siguiente paso será los try/catch.

---
+++ src/Ejercicio2/ClasePrincipal.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio2;
+
+/**
+ * Este programa realiza operaciones a partir de las variables "x" e "y".
+ * 
+ * @author Adrián Arjona
+ * @version 1.0
+ */
+public class ClasePrincipal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+    }
+    
+}
---
+++ src/Ejercicio2/OperacionesMatematicas.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ejercicio2;
+
+/**
+ * Contiene métodos estáticos que permiten sumar, restar, multiplicar y 
+ * dividir los valores x e y.
+ *
+ * @author Adrián Arjona
+ * @version 1.0
+ */
+
+
+public class OperacionesMatematicas {
+    
+    // Declaración de variables
+    
+    private double x ;
+    private double y ;
+    
+    // métodos
+    
+    /**
+     * Suma los valores "x" e "y".
+     * @param x Variable x.
+     * @param y Variable y.
+     * @return Resultado de la suma.
+     */
+    public static double suma(double x, double y)
+    {
+        double resultado = x + y;
+        return resultado ;
+    }
+    
+    /**
+     * Resta los valores "x" e "y".
+     * @param x Variable x.
+     * @param y Variable y que restará a x.
+     * @return Resultado de la resta.
+     */
+    public static double resta(double x, double y)
+    {
+        double resultado = x + y;
+        return resultado ;
+    }
+    
+    /**
+     * Multiplica los valores "x" e "y".
+     * @param x Variable x.
+     * @param y Variable y.
+     * @return Resultado de la multiplicación.
+     */
+    public static double multiplica(double x, double y)
+    {
+        double resultado = x * y;
+        return resultado ;
+    }
+    
+    /**
+     * Divide los valores "x" e "y".
+     * @param x Número a dividir.
+     * @param y Número por el que se divide x.
+     * @return Resultado de la división.
+     */
+    public static double divide(double x, double y)
+    {
+        double resultado = x / y;
+        return resultado ;
+    }
+}
